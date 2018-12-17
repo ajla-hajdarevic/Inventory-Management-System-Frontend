94c677e16e98e8dac3d551d1ac3a84c5178dc7c0
Zemka <azemina.celebic@stu.ssst.edu.ba>
2018-12-17 18:24:24 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import {HttpClientModule} from "@angular/common/http";
+import {LoginComponent} from "./login/login.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UiModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -3,11 +3,20 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import {RouterModule} from "@angular/router";
+import {LoginComponent} from "../login/login.component";
+import { InventoryListComponent } from './inventory-list/inventory-list.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
-  declarations: [LayoutComponent, HeaderComponent, FooterComponent],
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent, InventoryListComponent, FormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot([
+      {path: '', component: LoginComponent},
+      {path: 'inventorylist', component: InventoryListComponent},
+      {path: 'addform', component: FormComponent}
+    ])
   ],
   exports: [LayoutComponent]
 
